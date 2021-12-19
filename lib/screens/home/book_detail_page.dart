20cbe046e54a7d333e2947ba48fb268e4325f9aa
Amit Kumar <46594792+ak672676@users.noreply.github.com>
2021-12-19 21:34:09 +0530

    Book Detail page basic structure

--- lib/screens/home/book_detail_page.dart
+++ lib/screens/home/book_detail_page.dart
@@ -9,18 +9,56 @@ class BookDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text('Books'),
         backgroundColor: Colors.black,
-        iconTheme: IconThemeData(color: Colors.white),
-      ),
-      body: Center(
-        child: Text(
-          book.author,
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          title: Text('Books'),
+          backgroundColor: Colors.black,
+          iconTheme: IconThemeData(color: Colors.white),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Image.network(
+                  book.bookImage,
+                  height: 300,
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                book.bookTitle,
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.amber),
+              ),
+              Text(
+                book.author,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.white),
+              ),
+              SizedBox(height: 5),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+              ),
+              SizedBox(height: 20),
+              // Add Category
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+              ),
+              SizedBox(height: 20),
+              Text(book.description,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white)),
+            ],
+          ),
+        ));
   }
 }
